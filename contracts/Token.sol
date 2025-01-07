b2ce3fdd287d6e3d356bd4dd5b8f40d0f29bca65
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-01-06 23:57:13 -0500

    fix: fixi token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-contract Token {}
+contract Token is ERC20 {
+    address payable public owner;
+    address public creator;
+
+    constructor(address _creator, string memory _name, string memory _symbol, uint256 _totalSupply)
+        ERC20(_name, _symbol)
+    {
+        owner = payable(msg.sender);
+        creator = _creator;
+        _mint(msg.sender, _totalSupply);
+    }
+}

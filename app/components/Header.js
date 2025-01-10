da166c492b650a56f6e346a840d981c504011aef
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-01-10 01:44:56 -0500

    feat: finished frotend and test

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,27 @@
-import { ethers } from "ethers"
+import { ethers } from "ethers";
 
 function Header({ account, setAccount }) {
-
+  async function connectWallet() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.getAddress(accounts[0]);
+    setAccount(account);
+  }
   return (
     <header>
-
+      <p className="brand" style={{ padding: "1em" }}>
+        Meme.Mania
+      </p>
+      {account ? (
+        <button className="btn--fancy">
+          [{account.slice(0, 6) + "..." + account.slice(38, 42)}]
+        </button>
+      ) : (
+        <button onClick={connectWallet} className="btn--fancy">
+          Connect Wallet
+        </button>
+      )}
     </header>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,26 +1,125 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Header from "./components/Header"
-import List from "./components/List"
-import Token from "./components/Token"
-import Trade from "./components/Trade"
+import Header from "./components/Header";
+import List from "./components/List";
+import Token from "./components/Token";
+import Trade from "./components/Trade";
 
 // ABIs & Config
-import Factory from "./abis/Factory.json"
-import config from "./config.json"
-import images from "./images.json"
+import Factory from "./abis/Factory.json";
+import config from "./config.json";
+import images from "./images.json";
 
 export default function Home() {
+  const [provider, setProvider] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [factory, setFactory] = useState(null);
+  const [fee, setFee] = useState(0);
+  const [showCreate, setShowCreate] = useState(false);
+  const [token, setToken] = useState(null);
+  const [tokens, setTokens] = useState([]);
+  const [showTrade, setShowTrade] = useState(false);
+
+  function togleCreate() {
+    setShowCreate(!showCreate);
+  }
+  function toggleTrade(token) {
+    setToken(token);
+    setShowTrade(!showTrade);
+  }
+
+  async function loadBlockChainData() {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+
+    const factory = new ethers.Contract(
+      config[network.chainId].factory.address,
+      Factory,
+      provider
+    );
+
+    setFactory(factory);
+
+    const fee = await factory.fee();
+    setFee(fee);
+
+    const totalTokens = await factory.totalTokens();
+    const tokens = [];
+
+    for (let i = 0; i < totalTokens; i++) {
+      if (i == 6) {
+        break;
+      }
+      const tokenSale = await factory.getTokenSale(i);
+
+      const token = {
+        token: tokenSale.token,
+        name: tokenSale.name,
+        creator: tokenSale.creator,
+        sold: tokenSale.sold,
+        raised: tokenSale.raised,
+        isOpen: tokenSale.isOpen,
+        image: images[i],
+      };
+      tokens.push(token);
+    }
+    console.log(tokens);
+    setTokens(tokens.reverse());
+  }
+  useEffect(() => {
+    loadBlockChainData();
+  }, []);
 
   return (
     <div className="page">
-
-      <h1 style={{ padding: "1em" }}>fun.pump</h1>
-
+      <Header account={account} setAccount={setAccount} />
+      <main>
+        <div className="create">
+          <button onClick={togleCreate} className="btn--fancy">
+            {!factory
+              ? "[Contract not Deployed]"
+              : !account
+              ? "[Connect Wallet]"
+              : "[Crete New Token]"}
+          </button>
+        </div>
+        <div className="listings">
+          <h1>new listings</h1>
+          <div className="tokens">
+            {!account ? (
+              <p>Please conect the walelt</p>
+            ) : tokens.length === 0 ? (
+              <p>Not token listed</p>
+            ) : (
+              tokens.map((token, index) => (
+                <Token toggleTrade={toggleTrade} token={token} key={index} />
+              ))
+            )}
+          </div>
+        </div>
+      </main>
+      {showCreate && (
+        <List
+          toggleCreate={togleCreate}
+          provider={provider}
+          factory={factory}
+          fee={fee}
+        />
+      )}
+      {showTrade && (
+        <Trade
+          toggleTrade={toggleTrade}
+          provider={provider}
+          factory={factory}
+          account={account}
+          token={token}
+        />
+      )}
     </div>
   );
 }
